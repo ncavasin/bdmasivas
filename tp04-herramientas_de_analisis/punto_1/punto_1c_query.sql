f264ae0ba8699a288165a62255161c90abe60bd4
ncavasin <ncavasin97@gmail.com>
2020-10-19 17:20:11 -0300

    Pusheo antes de modificar muchas cosas

--- tp04-herramientas_de_analisis/punto_1/punto_1c_query.sql
+++ tp04-herramientas_de_analisis/punto_1/punto_1c_query.sql
@@ -1,3 +1,7 @@
+
+=============================================
+QUERY CON NORMALIZACION PARA LISTADO
+=============================================
 SELECT m.nombre AS "Nombre"
 	, t.descripcion AS "Tipo de medio"
 	, e.descripcion AS "Especialidad"
@@ -13,8 +17,28 @@ FROM medios AS m
 			ON m.id_especialidad = e.id_especialidad
 				INNER JOIN tipos_medio AS t
 				ON t.id_tipo = m.id_tipo_medio
-WHERE m.nombre LIKE 'm%' OR m.nombre LIKE 'M%'
+WHERE m.nombre LIKE 'M%'
 ORDER BY m.nombre ASC
+
+=============================================
+QUERY CON CANTIDAD PARA GRAFICO
+=============================================
+SELECT p.nombre AS "Provincia"
+	, t.descripcion AS "Tipo de Medio"
+	, COUNT (m.nombre) as "Cantidad"
+FROM medios AS m
+	INNER JOIN ciudades AS c
+	ON c.id_ciudad = m.id_ciudad
+		INNER JOIN provincias AS p
+		ON p.id_provincia = c.id_provincia
+			INNER JOIN especialidades AS e
+			ON m.id_especialidad = e.id_especialidad
+				INNER JOIN tipos_medio AS t
+				ON t.id_tipo = m.id_tipo_medio
+WHERE m.nombre LIKE 'M%'
+GROUP BY p.nombre, t.descripcion
+ORDER BY p.nombre
+
 
 
 
